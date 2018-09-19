59838abcbf9c15944a82d26119fcccf363dae129
Julio Domínguez <43100376+Struma@users.noreply.github.com>
2018-09-18 23:36:13 -0400

    Update view_GIF_frames.py

--- view_GIF_frames.py
+++ view_GIF_frames.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
+
 # Reading an animated GIF file using Python Image Processing Library - Pillow
+# I got this code from the internet, I think from stack exchange.
 
 from PIL import Image
-
 from PIL import GifImagePlugin
-
 import sys
-
-#argv[1] = "filename of gif in folder"
  
 
 imageObject = Image.open(sys.argv[1])
-
 print(imageObject.is_animated)
-
 print(imageObject.n_frames)
 
- 
-
-# Display individual frames from the loaded animated GIF file
-
 for frame in range(0,imageObject.n_frames):
-
     imageObject.seek(frame)
-
     imageObject.show() 
